965a3c4169534999a39c29de45099e0b310e168d
Jody <dantescreed@gmail.com>
2013-04-29 22:32:57 +0100

    Adding styling, token auto registration and deletion

--- block_kaplan_plugin.php
+++ block_kaplan_plugin.php
@@ -8,7 +8,7 @@ class block_kaplan_plugin extends block_base {
     }
 
     function get_content() {
-        global $CFG, $OUTPUT;
+        global $CFG, $OUTPUT, $USER, $DB;
 
         if ($this->content !== null) {
             return $this->content;
@@ -19,57 +19,109 @@ class block_kaplan_plugin extends block_base {
             return $this->content;
         }
 
-        $wsurl = $CFG->wwwroot . '/webservice/rest/server.php';
-
-
-        $coursesurl = $wsurl;
-        $coursesurl .= '?wsfunction=block_kaplan_plugin_get_courses_custom';
-        $coursesurl .= '&moodlewsrestformat=json';
-        $coursesurl .= '&wstoken=443f6ba99e6e86ffc2fca447eebd1e49';
-
-        $usersurl = $wsurl;
-        $usersurl .= '?wsfunction=block_kaplan_plugin_get_users_custom';
-        $usersurl .= '&moodlewsrestformat=json';
-        $usersurl .= '&wstoken=443f6ba99e6e86ffc2fca447eebd1e49';
-
         $this->content = new stdClass();
         $this->content->items = array();
         $this->content->icons = array();
         $this->content->footer = '';
 
-        // user/index.php expect course context, so get one if page has module context.
-        // $currentcontext = $this->page->context->get_course_context(false);
+        $context = context_system::instance();
+
+        //check if the service exists and is enabled
+        $service = $DB->get_record('external_services', array('component' => 'block_kaplan_plugin', 'enabled' => 1));
+        if (empty($service)) {
+            // will print message if no service found
+            $this->content->text = '<p>Notice: Kaplan plugin webservice not found</p>';
+            return $this->content;
+        }
+
+        //Get user token if it exists for this session already
+        $token = $DB->get_record('external_tokens', array('userid'=> $USER->id, 'externalserviceid' => $service->id, 'sid' => session_id()));
+
+        //No token? can we create one?
+        if(empty($token) && has_capability('moodle/webservice:createtoken', $context)) {
+            
+            //Delete old tokens for old sessions
+            $oldtokensql = "select * from {external_tokens}
+                    where userid = $USER->id 
+                    and externalserviceid = $service->id 
+                    and sid != '" . session_id() . "'";
+            $tokens = $DB->get_records_sql($oldtokensql);
+
+            foreach ($tokens as $t) {
+                $DB->delete_records('external_tokens', array('sid'=>$t->sid, 'userid' => $USER->id));
+            }
+
+            $newtoken = new stdClass;
+            if (empty($service->requiredcapability) || has_capability($service->requiredcapability, $context, $USER->id)) {
+                $newtoken->externalserviceid = $service->id;
+            } else {
+                $this->content->text = '<p class="kaplan_notice">Notice: You do not have access to use this service</p>';
+                return $this->content;
+            }
+
+            $numtries = 0;
+            do {
+                $numtries ++;
+                $generatedtoken = md5(uniqid(rand(),1));
+                if ($numtries > 5){
+                    $this->content->text = '<p class="kaplan_notice">Notice: Token generation failed</p>';
+                    return $this->content;
+                }
+            } while ($DB->record_exists('external_tokens', array('token'=>$generatedtoken)));
+
+            $newtoken->token = $generatedtoken;
+            $newtoken->tokentype = EXTERNAL_TOKEN_PERMANENT;
+            $newtoken->userid = $USER->id;
+            $newtoken->contextid = $context->id;
+            $newtoken->creatorid = $USER->id;
+            $newtoken->timecreated = time();
+            $newtoken->sid = session_id();
+            $newtoken->validuntil = 0;
+            $token = $newtoken;
+            $DB->insert_record('external_tokens', $newtoken);
+        } elseif (empty($token)) { //No token and unable to create one
+            $this->content->text = '<p class="kaplan_error">You must be logged in to view this information</p>';
+            return $this->content;
+        }
+        
+        //Base api url
+        $wsurl = $CFG->wwwroot . '/webservice/rest/server.php';
+
+        //Courses api url
+        $coursesurl = $wsurl;
+        $coursesurl .= '?wsfunction=block_kaplan_plugin_get_courses_custom';
+        $coursesurl .= '&moodlewsrestformat=json';
+        $coursesurl .= '&wstoken=' . $token->token;
+
+        //Users api url
+        $usersurl = $wsurl;
+        $usersurl .= '?wsfunction=block_kaplan_plugin_get_users_custom';
+        $usersurl .= '&moodlewsrestformat=json';
+        $usersurl .= '&wstoken=' . $token->token;
 
         if (! empty($this->config->text)) {
             $this->content->text = $this->config->text;
         }
 
+
+        // Add the tables and the loading gifs
         $this->content->text .= '<h3>Courses</h3>';
-        $this->content->text .= '<table id="kaplan_course_table">';
-        $this->content->text .= '<tr>';
-        $this->content->text .= '<th>Id</th><th>Name</th><th>Users enrolled</th>';
-        $this->content->text .= '</tr>';
+        $this->content->text .= '<table id="kaplan_course_table" class="kaplan_table">';
+        $this->content->text .= '<thead><tr>';
+        $this->content->text .= '<th class="kap_table_id">Id</th><th class="kap_table_name">Name</th><th class="kap_table_ue">Users enrolled</th>';
+        $this->content->text .= '</tr></thead><tbody></tbody>';
         $this->content->text .= '</table>';
         $this->content->text .= '<div class="courseloading_image"><img src="'.$CFG->wwwroot.'/pix/i/loading_small.gif"/></div>';
 
         $this->content->text .= '<h3>Users</h3>';
-        $this->content->text .= '<table id="kaplan_user_table">';
-        $this->content->text .= '<tr>';
+        $this->content->text .= '<table id="kaplan_user_table" class="kaplan_table">';
+        $this->content->text .= '<thead><tr>';
         $this->content->text .= '<th>Id</th><th>Fullname</th>';
-        $this->content->text .= '</tr>';
+        $this->content->text .= '</tr></thead><tbody></tbody>';
         $this->content->text .= '</table>';
         $this->content->text .= '<div class="userloading_image"><img src="'.$CFG->wwwroot.'/pix/i/loading_small.gif"/></div>';
-        // if (empty($currentcontext)) {
-        //     return $this->content;
-        // }
-        // if ($this->page->course->id == SITEID) {
-        //     $this->context->text .= "site context";
-        // }
 
-        if (! empty($this->config->text)) {
-            $this->content->text .= $this->config->text;
-        }
-
+        //Setup and call the js
         $this->page->requires->js('/blocks/kaplan_plugin/kaplan_plugin.js');
         $this->page->requires->js_function_call('kaplan_loadCourseTable', array('kaplan_course_table', $coursesurl));
         $this->page->requires->js_function_call('kaplan_loadUserTable', array('kaplan_user_table', $usersurl));
@@ -93,6 +145,7 @@ class block_kaplan_plugin extends block_base {
           return false;
     }
 
-    function has_config() {return flase;}
+    function has_config() {return false;}
+
 
 }
--- kaplan_plugin.js
+++ kaplan_plugin.js
@@ -13,15 +13,24 @@ function kaplan_loadCourseTable(eid, url) {
                     Y.log("JSON Parse failed!");
                     return;
                 }
+
+                if(courses instanceof Array === false) {
+                    Y.one('.courseloading_image').setHTML('<p class="kaplan_notice">Notice: You do not have access to use this service</p>');
+                    return;
+                }
+
                 for (i=0, l=courses.length; i < l; ++i) {
+
+                    var url = M.cfg.wwwroot + '/course/view.php?id=' + courses[i].id;
                 	html += '<tr>';
                     html += '<td>' + courses[i].id + '</td>';
-                    html += '<td>' + courses[i].fullname + '</td>';
-                    html += '<td>' + courses[i].users_enrolled + '</td>';
+                    html += '<td><a href="' + url + '">'+ courses[i].fullname + '</a></td>';
+                    html += '<td class="kap_table_ue">' + courses[i].users_enrolled + '</td>';
                     html += '</tr>';
                 }
                 Y.one('.courseloading_image').setStyle('display', 'none');
-                Y.one('#' + eid).append(html).setStyle('display', 'table');
+                Y.one('#' + eid + ' tbody').append(html);
+                Y.one('#' + eid).setStyle('display', 'table');
             },
             failure : function (x,o) {
                 Y.log("Async call failed!");
@@ -46,13 +55,22 @@ function kaplan_loadUserTable(eid, url) {
                     Y.log("JSON Parse failed!");
                     return;
                 }
+
+                if(users instanceof Array === false) {
+                    Y.one('.userloading_image').setHTML('<p class="kaplan_notice">Notice: You do not have access to use this service</p>');
+                    return;
+                }
+
                 for (i=0, l=users.length; i < l; ++i) {
+                    var url = M.cfg.wwwroot + '/user/profile.php?id=' + users[i].id;
                 	html += '<tr>'
-                    html += '<td>' + users[i].id + '</td><td>' +
-                            users[i].fullname + '</td></tr>';
+                    html += '<td>' + users[i].id + '</td>';
+                    html += '<td><a href="' + url + '">' + users[i].fullname + '</a></td>';
+                    html += '</tr>';
                 }
                 Y.one('.userloading_image').setStyle('display', 'none');
-                Y.one('#' + eid).append(html).setStyle('display', 'table');
+                Y.one('#' + eid).setStyle('display', 'table');
+                Y.one('#' + eid + ' tbody').append(html);
             },
             failure : function (x,o) {
                 Y.log("Async call failed!");
